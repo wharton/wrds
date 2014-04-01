30553661df63c30b4546cf5e8e80b9ce214e885f
Stein, Eric <ericst@wharton.upenn.edu>
2014-04-01 10:21:24 -0400

    created sql connection file

--- wrds/sql.py
+++ wrds/sql.py
@@ -7,14 +7,26 @@ import jaydebeapi
 import pandas
 
 class SQLConnection(object):
-    def __init__(self):
-        print "Enter your credentials."
-        self.username = raw_input("Username: ")
-        self.password = getpass.getpass()
+    def __init__(self, username=None, password=None):
+        if not username and not password:
+            print "Enter your credentials."
+            self.username = raw_input("Username: ")
+            self.password = getpass.getpass()
+        else:
+            self.username = username
+            self.password = password
+    
+    def connnect(self):
         try:
             self.conn = jaydebeapi.connect('com.sas.net.sharenet.ShareNetDrive',
                 ['jdbc:sharenet//wrds-triton.wharton.upenn.edu:8551/', self.username, self.password])
         except:
             print "Your username or password might be incorrect."
+
+    def sql(call, df=None, index=None):
+        if df:
+            pass
+        else:
+            return pandas.io.sql.read_sql(call, self.conn, index_col=index)
     
 
