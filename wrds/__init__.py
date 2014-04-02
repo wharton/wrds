c59086615bc7f2236ff1dfa1ede8186fef0218eb
Stein, Eric <ericst@wharton.upenn.edu>
2014-04-02 16:53:16 -0400

    added metadata functions

--- wrds/__init__.py
+++ wrds/__init__.py
@@ -7,10 +7,12 @@ WRDS Python Data Access Library
 WRDS-Py is a library for extracting data from WRDS data sources and getting it into Pandas.
 
     >>> import wrds
-    >>> data = wrds.sql('select * from Crsp.msi', index='DATE')
+    >>> connection = wrds.SQLConnection(username='user', password='password') 
+    >>> connection.get_libraries()
+    >>> connection.get_tables('CRSP', verbose=True)
+    >>> data = connection.sql('SELECT * FROM CRSP.MSI', index='DATE')
     >>> data.head()
 
-There will be more added to this, including additional data processing methods.
 
 """
 
@@ -19,5 +21,11 @@ __version__ = '0.1.0'
 __author__ = 'Eric Stein'
 __copyright__ = '2014 Wharton Research Data Services'
 
+import os
+
 from .sql import SQLConnection
 
+
+# set up the Java environment for the odbc drivers.
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/java'
+os.environ['CLASSPATH'] = '/usr/local/etc/sas_jdbc_driver/sas.core.jar:/usr/local/etc/sas_jdbc_driver/sas.intrnet.javatools.jar'
--- wrds/sql.py
+++ wrds/sql.py
@@ -17,11 +17,25 @@ class SQLConnection(object):
             self.password = password
     
     def connnect(self):
-        try:
-            self.conn = jaydebeapi.connect('com.sas.net.sharenet.ShareNetDrive',
-                ['jdbc:sharenet//wrds-triton.wharton.upenn.edu:8551/', self.username, self.password])
-        except:
-            print "Your username or password might be incorrect."
+        """
+            Connect to the sasshare. This will attempt to log in using the provided credentials.
+            If it is rejected, the login will try three more times.
+        """
+        attempt = 0
+        self.conn = None
+        while attempt < 3 and not self.conn:
+            try:
+                self.conn = jaydebeapi.connect('com.sas.net.sharenet.ShareNetDrive',
+                    ['jdbc:sharenet://wrds-triton.wharton.upenn.edu:8551/', self.username, self.password])
+                print "Success"
+                return 1
+            except:
+                print "Your username or password might be incorrect."
+                self.username = raw_input('Username: ')
+                self.password = getpass.getpass()
+                attempt += 1
+        print "You were unable to connect. Please contact support."
+        return 0
 
     def get_libraries(self):
         """
@@ -38,17 +52,37 @@ class SQLConnection(object):
             table name, the number of observations, and the description.
         """
         cursor = self.conn.cursor()
-        query = 'select memname, memlabel, nobs from dictionary.tables where libname = %s' % library
+        query = 'select memname, memlabel, nobs from dictionary.tables where libname = "%s";' % library
         cursor.execute(query)
+        results = cursor.fetchall()
+         
         if verbose:
-            output = [{'name': name.strip(), 'desc': label.strip(), 'obs': int(obs)} for name, label, obs in cursor.fetchall()]
+            output = [{'name': name.strip(), 'desc': label.strip(), 'obs': int(obs)} for name, label, obs in [row for row in results]]
         else:
-            output = [name[0].strip() for name in cursor.fetchall()]
+            output = [name[0].strip() for name in results]
 
         return output
 
+    def describe_table(self, library, table):
+        """
+            Takes the library and the table and describes all the columns in that table.
+            Includes Column Name, Column Type, Column Length, Column Label, Column Format, Is Null?.
+        """
+        cursor = self.conn.cursor()
+        query = '''select name, type, length, label, format, notnull
+                 from dictionary.columns
+                 WHERE libname = "{library}" AND memname="{table}";
+                 '''
+        cursor.execute(query.format(library=library, table=table))
+        results = cursor.fetchall()
+        output = [{'name': name, 'type': tpe, 'length': length, 'description': label, 'format': formt, 'notnull': notnull}
+                    for name, tpe, length, label, formt, notnull in map(lambda s: s.strip(), [entry for entry in results])] 
+        return output
 
-    def sql(self, all, df=None, index=None):
+    def sql(self, call, df=None, index=None):
+        """
+            This processes the SQL commands to a Pandas dataframe.
+        """
         if df:
             pass
         else:
