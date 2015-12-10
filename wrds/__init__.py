a230bae8fc5685dfee92f06e2f1eafcdf6a41c26
Dougherty, Joe <josepd@wharton.upenn.edu>
2015-12-10 15:46:19 -0500

    Adds a whole slew of additional sanity checks, particularly w/r/t AUTHFILE. We are now checking: if the correct variables are set in AUTHFILE, if the username/password combination produces a working connection, if classpath is populated and if the files specified by classpath already exist on the filesystem.

--- wrds/__init__.py
+++ wrds/__init__.py
@@ -44,7 +44,7 @@ def _parse_config(file_location):
 
     try:
         Config.read(file_location)
-        result = Config.get('credentials', 'username'), Config.get('credentials', 'password'), Config.get('credentials', 'classpath')
+        return Config.get('credentials', 'username'), Config.get('credentials', 'password'), Config.get('credentials', 'classpath')
     except:
         err = 'The .wrdsauthrc file appears to be improperly formatted.\n'
         err += 'Please check the format.\n'
@@ -54,10 +54,7 @@ def _parse_config(file_location):
         err += 'password=<password>\n'
         err += 'classpath=<set of paths to sas.core.jar and sas.intrnet.javatools.jar>\n\n'
         err += 'Please see https://wrds-web.wharton.upenn.edu/wrds/support/ for additional information.\n'
-        
-        print(err)
-        return False
-    return result
+        raise SyntaxError(err)
 
 def _usage():
     print('Please include a .wrdsauthrc file in your home directory formatted as below:\n')
@@ -80,20 +77,45 @@ def _greet_if_interactive():
     """
     import __main__ as main
     if not hasattr(main, '__file__'):
-        print('You are now connected with credentials found at ' + AUTHFILE + '.')
+        print('You are now connected with credentials found at {}.'.format(AUTHFILE))
         print('\'help(wrds)\' to learn how to interact with the system.')
     
-if os.path.isfile(AUTHFILE):
-    credentials = _parse_config(AUTHFILE)
-    if credentials: 
-        username, password, classpath = credentials
-        os.environ['CLASSPATH'] = classpath
+def _verify_credentials(username, password):
+    try:
         if _authenticate(username, password):
             _greet_if_interactive()
         else:
-            print('The credentials found in ' + AUTHFILE + ' could not be used to authenticate.')
-            print('Please provide your username and password.')
-            _manual_connect()
+            err = '\nThe credentials found in {} could not be used to authenticate.\n'.format(AUTHFILE)
+            err += 'Please verify that the username and password are correct.'
+            raise OSError(err)
+    except:
+        print('Please verify that classpath has been set correctly in {}'.format(AUTHFILE))
+
+def _verify_and_set_classpath(classpath):
+    # Classpath should be two paths separated by a colon
+    # Try to split on : and check that both paths exist
+    paths_to_check = classpath.split(':')
+
+    if paths_to_check == ['']: # The user may have not included any paths in the classpath section
+        err = '\nPlease specify paths for the JDBC drivers in the classpath section of {}'.format(AUTHFILE)
+        raise SyntaxError(err)
+
+    for path in paths_to_check:
+        if not os.path.exists(path):
+            err = '\nCould not the driver specified at {}\n'.format(path)
+            err += 'Please check that classpath is set correctly in {}.'.format(AUTHFILE)
+            raise SystemError(err)
+
+    # If classpaths exist, set the appropriate environment variable
+    os.environ['CLASSPATH'] = classpath
+
+def _verify_classpath_and_credentials(config_settings):
+    username, password, classpath = config_settings
+    _verify_and_set_classpath(classpath)               
+    _verify_credentials(username, password)
+
+if os.path.isfile(AUTHFILE):
+    _verify_classpath_and_credentials(_parse_config(AUTHFILE))
 else:
     _usage()
 
