3ff5334f8a8b099ca03a2c0730f985290c157fb7
Joe Dougherty <joseph.dougherty@gmail.com>
2017-01-26 11:53:24 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
         name="wrds",
-        version='0.0.5',
+        version='0.0.6',
         description="Read SAS datasets remotely (from wrds-cloud) into a Pandas dataframe.",
         author="Joe Dougherty",
         author_email="josepd@wharton.upenn.edu",
