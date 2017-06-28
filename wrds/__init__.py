f7a296e8065fc1ae74b008165d8b85a94dfb91c7
Joe Dougherty <joseph.dougherty@gmail.com>
2017-06-28 16:18:20 -0400

    Removes 'username' and 'password' params from module-level docstring as these are no longer supported.

--- wrds/__init__.py
+++ wrds/__init__.py
@@ -7,7 +7,7 @@ WRDS Python Data Access Library
 WRDS-Py is a library for extracting data from WRDS data sources and getting it into Pandas.
 
     >>> import wrds
-    >>> connection = wrds.Connection(username='user', password='password') 
+    >>> connection = wrds.Connection()
     >>> connection.get_libraries()
     >>> connection.get_tables('CRSP', verbose=True)
     >>> data = connection.sql('SELECT * FROM CRSP.MSI', index='DATE')
