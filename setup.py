cf9b85853488a2a3e51820c080c38dc66955e8c0
Eric Stein <ericst@wharton.upenn.edu>
2019-04-23 12:51:35 -0400

    Fixes dependancy on psycopg2

--- setup.py
+++ setup.py
@@ -17,39 +17,39 @@ packages = [
 requires = [
     'pandas',
     'sqlalchemy',
-    'psycopg2',
-     # mock may need to be included if folks
-     # want to run tests w/Py2
+    'psycopg2-binary',
+    # mock may need to be included if folks
+    # want to run tests w/Py2
     'mock'
 ]
 
 setup(
-        name='wrds',
-        version='3.0.6',
-        description="Python access to WRDS Data",
-        long_description=open('README.rst').read(),
-        author='WRDS',
-        author_email='wrds@wharton.upenn.edu',
-        url='http://www.whartonwrds.com',
-        packages=packages,
-        install_requires=requires,
-        package_data={
-            '': ['LICENSE', 'NOTICE', '*.rst'],
-            },
-        classifiers=(
-           'Programming Language :: Python',
-           'Programming Language :: Python :: 2.7',
-           'Programming Language :: Python :: 3',
-           'Programming Language :: Python :: 3.5',
-           'Development Status :: 4 - Beta',
-           'Environment :: Console',
-           'Intended Audience :: End Users/Desktop',
-           'Intended Audience :: Financial and Insurance Industry',
-           'Intended Audience :: Education',
-           'Intended Audience :: Science/Research',
-           'Topic :: Office/Business :: Financial',
-           'Topic :: Scientific/Engineering :: Information Analysis',
-        ),
+    name='wrds',
+    version='3.0.7',
+    description="Python access to WRDS Data",
+    long_description=open('README.rst').read(),
+    author='WRDS',
+    author_email='wrds@wharton.upenn.edu',
+    url='http://www.whartonwrds.com',
+    packages=packages,
+    install_requires=requires,
+    package_data={
+        '': ['LICENSE', 'NOTICE', '*.rst'],
+    },
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ),
 )
 
 
