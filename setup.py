c8a6019532c76aa4019f8b7d0c12ca93bbe9d0e7
VanSickle, Jeffrey <vjef@wharton.upenn.edu>
2021-03-03 17:42:02 -0500

    Update minor version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ requires = [
 
 setup(
     name='wrds',
-    version='3.0.8',
+    version='3.0.10',
     description="Python access to WRDS Data",
     long_description=open('README.rst').read(),
     author='WRDS',
