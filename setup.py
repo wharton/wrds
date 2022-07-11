bbd0cde9ca5479d84a29d418e960bb6f17f6d94d
Alex Malek <amalek@wharton.upenn.edu>
2022-07-11 12:04:31 -0400

    bump minor version to 3.1.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requires = [
 
 setup(
     name='wrds',
-    version='3.1.1',
+    version='3.1.2',
     description="Python access to WRDS Data",
     long_description=open('README.rst').read(),
     author='WRDS',
